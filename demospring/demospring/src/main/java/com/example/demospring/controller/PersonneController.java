4d7da236d571a17e9c9d7e9737e94ba87bf546ad
Christophe <you@example.com>
2022-12-21 14:49:58 +0100

    exercice 2

--- demospring/demospring/src/main/java/com/example/demospring/controller/PersonneController.java
+++ demospring/demospring/src/main/java/com/example/demospring/controller/PersonneController.java
@@ -4,6 +4,7 @@ package com.example.demospring.controller;
 import com.example.demospring.entity.Personne;
 import com.example.demospring.interfaces.IPersonneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,7 @@ import java.util.List;
 public class PersonneController {
 
     @Autowired
+    @Qualifier("personne2Service")
     private IPersonneService personneService;
 
     @GetMapping("")
