f1142a4372df3d069553f48090c37cf7c4d4f443
Christophe <you@example.com>
2022-12-22 15:58:22 +0100

    debut correction todolist

---
+++ TpTodo/tptodolist/src/main/java/com/example/tptodolist/TptodolistApplication.java
@@ -0,0 +1,13 @@
+package com.example.tptodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TptodolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TptodolistApplication.class, args);
+    }
+
+}
